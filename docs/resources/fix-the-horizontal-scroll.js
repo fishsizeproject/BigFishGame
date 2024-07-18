0866c690eeebd9f07cb30ff4180ca39279921994
Tormey Reimer <tormeyreimer@gmail.com>
2024-07-18 22:17:07 +1000

    Render with minor changes

---
+++ docs/resources/fix-the-horizontal-scroll.js
@@ -0,0 +1,7 @@
+function setVw() {
+  let vw = document.documentElement.clientWidth / 100;
+  document.documentElement.style.setProperty('--vw', `${vw}px`);
+}
+
+setVw();
+window.addEventListener('resize', setVw);
